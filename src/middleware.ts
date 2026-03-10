89541fa1a63cc00331e103be735cb5886d0626d4
PraDeePKuMaR <pradeepkumar24rk@gmail.com>
2026-03-10 11:05:55 +0530

    routing issue solved

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,13 @@ import { defineMiddleware } from 'astro/middleware';
 
 export const onRequest = defineMiddleware((context, next) => {
   const url = new URL(context.request.url);
-
   if (url.pathname.startsWith('/order/') && !url.pathname.startsWith('/__fallback__/order')) {
     url.pathname = '/__fallback__/order';
     return context.rewrite(url);
   }
-
+  if (url.pathname.startsWith('/products/') && !url.pathname.startsWith('/__fallback__/products')) {
+    url.pathname = '/__fallback__/products';
+    return context.rewrite(url);
+  }
   return next();
 });
